1dfa4c3a2fc3443be165462b9d531c87ccd7a0a6
Ashanti Hatchett <ahatchett02@gmail.com>
2024-10-03 19:53:23 +0000

    fixed update

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -39,17 +39,17 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description", the_delivery.description)
     the_delivery.details = params.fetch("query_details", the_delivery.details)
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on", the_delivery.supposed_to_arrive_on)
-    the_delivery.arrived = params.fetch("query_arrived")
+    
+    the_delivery.arrived = params.fetch("arrived", false)
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Moved to Received" })
+      redirect_to("/deliveries/", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries/", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
